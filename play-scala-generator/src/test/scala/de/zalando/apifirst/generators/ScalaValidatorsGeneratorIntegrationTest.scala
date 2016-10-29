70a79c035524fce27b067e2cf1827b28c244f2d0
slasch <slavaschmidt@gmx.de>
2016-10-29 13:51:39 +0200

    FIX: missing imports in test data generator

--- play-scala-generator/src/test/scala/de/zalando/apifirst/generators/ScalaValidatorsGeneratorIntegrationTest.scala
+++ play-scala-generator/src/test/scala/de/zalando/apifirst/generators/ScalaValidatorsGeneratorIntegrationTest.scala
@@ -1,8 +1,8 @@
 package de.zalando.apifirst.generators
 
 import de.zalando.ExpectedResults
-import de.zalando.model.{WithModel, all_of_imports_yaml}
-import org.scalatest.{FunSpec, MustMatchers}
+import de.zalando.model.{ WithModel, all_of_imports_yaml }
+import org.scalatest.{ FunSpec, MustMatchers }
 
 class ScalaValidatorsGeneratorIntegrationTest extends FunSpec with MustMatchers with ExpectedResults {
 
